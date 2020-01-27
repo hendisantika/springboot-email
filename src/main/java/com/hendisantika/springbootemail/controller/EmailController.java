0959fae0b49ae2b9e3fb2251d08a1e187c6efa37
Hendi Santika <hendisantika@gmail.com>
2020-01-28 06:31:50 +0700

    #9 - Add EmailController

---
+++ src/main/java/com/hendisantika/springbootemail/controller/EmailController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootemail.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-email
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/01/20
+ * Time: 06.31
+ */
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+}
