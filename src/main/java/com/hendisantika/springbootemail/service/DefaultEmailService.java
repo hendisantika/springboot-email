39fcdbaf14b9f816db7cc10c269cdd2d26eacc89
Hendi Santika <hendisantika@gmail.com>
2020-01-28 06:30:51 +0700

    #7 - Add sendSimpleEmail in DefaultEmailService

--- src/main/java/com/hendisantika/springbootemail/service/DefaultEmailService.java
+++ src/main/java/com/hendisantika/springbootemail/service/DefaultEmailService.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootemail.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +18,13 @@ import org.springframework.stereotype.Service;
 public class DefaultEmailService implements EmailService {
     @Autowired
     public JavaMailSender emailSender;
+
+    @Override
+    public void sendSimpleEmail(String toAddress, String subject, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(toAddress);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+        emailSender.send(simpleMailMessage);
+    }
 }
