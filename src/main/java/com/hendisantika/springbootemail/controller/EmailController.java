1098882ae6be26c3480fe0fd99e86ae85ef39b42
Hendi Santika <hendisantika@gmail.com>
2020-01-28 06:32:41 +0700

    #11 - Add sendSimpleEmail in EmailController

--- src/main/java/com/hendisantika/springbootemail/controller/EmailController.java
+++ src/main/java/com/hendisantika/springbootemail/controller/EmailController.java
@@ -4,7 +4,13 @@ import com.hendisantika.springbootemail.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,4 +29,19 @@ public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+    @GetMapping(value = "/simple-email/{user-email}")
+    public @ResponseBody
+    ResponseEntity<String> sendSimpleEmail(@PathVariable("user-email") String email) {
+
+        try {
+            emailService.sendSimpleEmail(email, "Welcome", "This is a welcome email for your!!");
+        } catch (MailException mailException) {
+            LOG.error("Error while sending out email..{}", mailException.getStackTrace());
+            LOG.error("Error while sending out email..{}", mailException.fillInStackTrace());
+            return new ResponseEntity<>("Unable to send email", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>("Please check your inbox", HttpStatus.OK);
+    }
 }
