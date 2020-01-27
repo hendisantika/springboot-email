1e2b4cf211b7301d280858a0bb50101d48b840e7
Hendi Santika <hendisantika@gmail.com>
2020-01-28 06:31:10 +0700

    #8 - Add sendEmailWithAttachment in DefaultEmailService

--- src/main/java/com/hendisantika/springbootemail/service/DefaultEmailService.java
+++ src/main/java/com/hendisantika/springbootemail/service/DefaultEmailService.java
@@ -1,9 +1,16 @@
 package com.hendisantika.springbootemail.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.FileNotFoundException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +33,17 @@ public class DefaultEmailService implements EmailService {
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(message);
         emailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment) throws MessagingException, FileNotFoundException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+        messageHelper.setTo(toAddress);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(message);
+        FileSystemResource file = new FileSystemResource(ResourceUtils.getFile(attachment));
+        messageHelper.addAttachment("Purchase Order", file);
+        emailSender.send(mimeMessage);
     }
 }
